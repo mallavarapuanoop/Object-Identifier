2a4bdf1063aa48f7870637953af8ced28b3c2829
Anoop Mallavarapu <mallavarapuanoop@gmail.com>
2018-12-05 14:57:25 -0500

    Mysecond commit

--- OB/ViewController.swift
+++ OB/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     let messageString = "Hello"
     
+    var new = "Hii"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

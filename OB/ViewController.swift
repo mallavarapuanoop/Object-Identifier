8acf064a9acd3f25cd5e28c2ed892d8084a69913
Anoop Mallavarapu <mallavarapuanoop@gmail.com>
2018-12-05 14:54:33 -0500

    my first changes

---
+++ OB/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OB
+//
+//  Created by Anoop Mallavarapu on 12/5/18.
+//  Copyright © 2018 Anoop Mallavarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let messageString = "Hello"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
